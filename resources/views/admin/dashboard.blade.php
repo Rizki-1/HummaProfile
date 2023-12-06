3a9c4ca2d597bfe6b5690b1253065f6bddbfffdb
Rizki-1 <143168532+Rizki-1@users.noreply.github.com>
2023-12-06 11:39:02 +0700

    dashboard admin logo

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -6,22 +6,22 @@
     <div class="col-md-4">
       <div class="card text-white bg-primary h-100 d-flex flex-row align-items-center">
         <div class="card-body">
-          <i class="fa-solid fa-people-roof fa-3x"></i>
+            <i class="link-icon" data-feather="box"></i>
         </div>
         <div class="card-body text-end">
-          <h5 class="card-title">Jumlah Siswa Magang</h5>
+          <h5 class="card-title">Jumlah Produk</h5>
           <p class="card-text">{{ $produkCount }}</p>
         </div>
       </div>
     </div>
 
     <div class="col-md-4">
-      <div class="card text-white bg-danger h-100 d-flex flex-row align-items-center">
+      <div class="card text-white bg-info h-100 d-flex flex-row align-items-center">
         <div class="card-body">
-          <i class="fa fa-ticket fa-3x"></i>
+            <i class="link-icon" data-feather="book-open"></i>
         </div>
         <div class="card-body text-end">
-          <h5 class="card-title">Industri</h5>
+          <h5 class="card-title">Jumlah Berita</h5>
           <p class="card-text">{{ $beritaCount }}</p>
         </div>
       </div>
@@ -30,10 +30,10 @@
     <div class="col-md-4">
       <div class="card text-white bg-success h-100 d-flex flex-row align-items-center">
         <div class="card-body">
-          <i class="fa fa-database fa-3x"></i>
+            <i class="link-icon" data-feather="inbox"></i>
         </div>
         <div class="card-body text-end">
-          <h5 class="card-title">Data</h5>
+          <h5 class="card-title">Jumlah inbox</h5>
           <p class="card-text">{{ $inboxCount }}</p>
         </div>
       </div>
