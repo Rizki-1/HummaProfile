599c7a5864df32449d3b2832913cba018df3708d
lintangpermata <permatalintang29@gmail.com>
2023-12-07 15:52:08 +0700

    errors, testimoni, color

---
+++ resources/views/errors/405.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>405</title>
+    <link rel="stylesheet" href="{{ asset('cssUser/css/error.css') }}">
+    <link rel="stylesheet" href="{{ asset('cssUser/css/plugins/bootstrap.min.css') }}">
+</head>
+<body>
+    <div class="styleerror">
+        <img src="{{ asset('cssUser/images/illustration/405.png') }}" alt="405-error" class="mb-4">
+        <span class="title mb-2">405 - METHOD NOT ALLOWED</span>
+        <p>Permintaan tidak dapat diproses. Metode tidak diizinkan untuk sumber daya ini.</p>
+        <button class="btn btn-primary">Kembali</button>
+    </div>
+</body>
+</html>
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>500</title>
+    <link rel="stylesheet" href="{{ asset('cssUser/css/error.css') }}">
+    <link rel="stylesheet" href="{{ asset('cssUser/css/plugins/bootstrap.min.css') }}">
+</head>
+<body>
+    <div class="styleerror">
+        <img src="{{ asset('cssUser/images/illustration/500.png') }}" alt="500-error" class="mb-4">
+        <span class="title mb-2">500 - INTERNAL SERVER ERROR</span>
+        <p>Maaf, terjadi kesalahan internal pada server. Kami akan segera mengatasi masalah ini. <br> Silakan coba lagi nanti.</p>
+    </div>
+</body>
+</html>
