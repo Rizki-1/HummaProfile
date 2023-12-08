3eddfa64be4f63af821855f7d53fba74e59f3daa
Rizki-1 <143168532+Rizki-1@users.noreply.github.com>
2023-12-08 13:47:10 +0700

    request update galery

--- app/Http/Requests/UpdateGalleryRequest.php
+++ app/Http/Requests/UpdateGalleryRequest.php
@@ -11,7 +11,7 @@ class UpdateGalleryRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,15 @@ class UpdateGalleryRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'picture' => 'required|image|mimes:png,jpg',
+            'target_layanan_id' => 'required|exists:target_layanans,id',
+        ];
+    }
+
+    public function messages():array
+    {
+        return [
+
         ];
     }
 }
