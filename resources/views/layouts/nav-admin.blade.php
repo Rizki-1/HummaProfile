91c3bfbac453437f89a433678737123aac0e2564
Ahmad Ilyas Fitono <140124534+IlyasGaming@users.noreply.github.com>
2023-12-08 14:03:49 +0700

    Perbaikan kata"

--- resources/views/layouts/nav-admin.blade.php
+++ resources/views/layouts/nav-admin.blade.php
@@ -11,8 +11,10 @@
   <link rel="preconnect" href="https://fonts.googleapis.com/">
   <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&amp;display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/7.3.67/css/materialdesignicons.min.css" integrity="sha512-nRzny9w0V2Y1/APe+iEhKAwGAc+K8QYCw4vJek3zXhdn92HtKt226zHs9id8eUq+uYJKaH2gPyuLcaG/dE5c7A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/7.3.67/css/materialdesignicons.min.css" integrity="sha512-nRzny9w0V2Y1/APe+iEhKAwGAc+K8QYCw4vJek3zXhdn92HtKt226zHs9id8eUq+uYJKaH2gPyuLcaG/dE5c7A=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous"
+    referrerpolicy="no-referrer" />
   <!-- End fonts -->
 
   <!-- core:css -->
@@ -78,8 +80,7 @@
 
           <li class="nav-item nav-category">List</li>
           <li class="nav-item">
-            <a class="nav-link" data-bs-toggle="collapse" href="#list" role="button" aria-expanded="false"
-              aria-controls="berita">
+            <a class="nav-link" data-bs-toggle="collapse" href="#list" role="button" aria-expanded="false" aria-controls="berita">
               <i class="link-icon" data-feather="grid"></i>
               <span class="link-title">List</span>
               <i class="link-arrow" data-feather="chevron-down"></i>
@@ -93,10 +94,10 @@
                   <a href="{{ route('list.kelas_industri') }}" class="nav-link">Kelas Industri</a>
                 </li> --}}
                 <li class="nav-item">
-                    <a href={{ route('mou.index') }} class="nav-link">Mou</a>
+                  <a href={{ route('mou.index') }} class="nav-link">Mou</a>
                 </li>
                 <li class="nav-item">
-                    <a href={{ route('testimoni.index') }} class="nav-link">Testimoni</a>
+                  <a href={{ route('testimoni.index') }} class="nav-link">Testimoni</a>
                 </li>
               </ul>
             </div>
@@ -125,8 +126,7 @@
 
           <li class="nav-item nav-category">Berita</li>
           <li class="nav-item">
-            <a class="nav-link" data-bs-toggle="collapse" href="#berita" role="button" aria-expanded="false"
-              aria-controls="berita">
+            <a class="nav-link" data-bs-toggle="collapse" href="#berita" role="button" aria-expanded="false" aria-controls="berita">
               <i class="link-icon" data-feather="copy"></i>
               <span class="link-title">Berita</span>
               <i class="link-arrow" data-feather="chevron-down"></i>
@@ -142,7 +142,6 @@
               </ul>
             </div>
           </li>
-
 
           <li class="nav-item nav-category">Gallery</li>
           <li class="nav-item">
@@ -170,8 +169,7 @@
 
           <li class="nav-item nav-category">Pengaturan</li>
           <li class="nav-item">
-            <a class="nav-link" data-bs-toggle="collapse" href="#pengaturan" role="button" aria-expanded="false"
-              aria-controls="pengaturan">
+            <a class="nav-link" data-bs-toggle="collapse" href="#pengaturan" role="button" aria-expanded="false" aria-controls="pengaturan">
               <i class="link-icon" data-feather="settings"></i>
               <span class="link-title">Pengaturan</span>
               <i class="link-arrow" data-feather="chevron-down"></i>
@@ -186,6 +184,9 @@
                 </li>
                 <li class="nav-item">
                   <a href="{{ route('cabang-perusahaan.index') }}" class="nav-link">Cabang Perusahaan</a>
+                </li>
+                <li class="nav-item">
+                  <a href="{{ route('syarat-dan-ketentuan.index') }}" class="nav-link">Syarat Dan Ketentuan</a>
                 </li>
               </ul>
             </div>
@@ -206,16 +207,13 @@
         <div class="navbar-content">
           <ul class="navbar-nav">
             <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="profileDropdown" role="button"
-                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <img class="wd-30 ht-30 rounded-circle" src="{{ asset('ImageGlobal/HummaTech-Logo.png') }}"
-                  alt="profile">
+              <a class="nav-link dropdown-toggle" href="#" id="profileDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <img class="wd-30 ht-30 rounded-circle" src="{{ asset('ImageGlobal/HummaTech-Logo.png') }}" alt="profile">
               </a>
               <div class="dropdown-menu p-0" aria-labelledby="profileDropdown">
                 <div class="d-flex flex-column align-items-center border-bottom px-5 py-3">
                   <div class="mb-3">
-                    <img class="wd-80 ht-80 rounded-circle" src="{{ asset('ImageGlobal/HummaTech-Logo.png') }}"
-                      alt="">
+                    <img class="wd-80 ht-80 rounded-circle" src="{{ asset('ImageGlobal/HummaTech-Logo.png') }}" alt="">
                   </div>
                   <div class="text-center">
                     <p class="tx-16 fw-bolder">{{ config('app.name') }}</p>
@@ -227,8 +225,8 @@
                     <form action="{{ route('logout') }}" method="post">
                       @csrf
                       <button type="submit" class="text-body ms-0" id="button-logout">
-                          <i class="me-2 icon-md" data-feather="log-out"></i>
-                          <span>Log Out</span>
+                        <i class="me-2 icon-md" data-feather="log-out"></i>
+                        <span>Log Out</span>
                       </button>
                     </form>
                   </li>
@@ -245,13 +243,11 @@
       </div>
 
       <!-- partial:partials/_footer.html -->
-      <footer
-        class="footer d-flex flex-column flex-md-row align-items-center justify-content-between px-4 py-3 border-top small">
+      <footer class="footer d-flex flex-column flex-md-row align-items-center justify-content-between px-4 py-3 border-top small">
         <p class="text-muted mb-1 mb-md-0">Copyright © 2023 <a
             href="https://www.google.com/search?q=hummatech&rlz=1C1FKPE_idID1058ID1058&oq=hummatech&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIPCAEQLhgKGMcBGNEDGIAEMgkIAhAAGAoYgAQyBggDEEUYPDIGCAQQRRg8MgYIBRBFGDzSAQgxNzIxajBqN6gCALACAA&sourceid=chrome&ie=UTF-8"
             target="_blank">{{ config('app.name') }}</a>.</p>
-        <p class="text-muted">Di buat oleh Hummaprofile <i class="mb-1 text-primary ms-1 icon-sm"
-            data-feather="heart"></i></p>
+        <p class="text-muted">Di buat oleh Hummaprofile <i class="mb-1 text-primary ms-1 icon-sm" data-feather="heart"></i></p>
       </footer>
       <!-- partial -->
 
@@ -259,15 +255,15 @@
   </div>
 
   <!-- endinject -->
-    @if (session('message'))
+  @if (session('message'))
     <script>
-        Swal.fire({
+      Swal.fire({
         icon: "{{ session('message')['icon'] ?? 'question' }}",
         title: "{{ session('message')['title'] ?? 'Tidak ada keteerangan' }}",
         text: "{{ session('message')['text'] ?? 'Tidak ada keterangan' }}",
-        })
+      })
     </script>
-    @endif
+  @endif
   <!-- Plugin js for this page -->
   <script src="{{ asset('cssAdmin/vendors/flatpickr/flatpickr.min.js') }}"></script>
   <script src="{{ asset('cssAdmin/vendors/apexcharts/apexcharts.min.js') }}"></script>
